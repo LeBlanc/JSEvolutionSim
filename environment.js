ccf5304c69ff1979f4015b0a683ae0cb8a0a136d
Andrew LeBlanc <andrew@naturalparsing.com>
2011-05-21 18:34:09 -0700

    change habitat attributes generators

--- environment.js
+++ environment.js
@@ -10,8 +10,8 @@ function Environment(canvas, x, y) {
 	
 	this.generate = function(canvas) {
 		var moisture = this.random_sources(10, 15, 350, -100);
-		var mountains = this.random_sources(20, 80, -150, 200);
-		var soil = this.random_sources(20, 40, 0, 100);
+		var mountains = this.random_sources(20, 80, 50, -20);
+		var soil = this.random_sources(20, 40, -10, 100);
 		var temperature = this.random_sources(20, 40, 200, -100);
 	
 		canvas = ($("#environment table tbody"));
